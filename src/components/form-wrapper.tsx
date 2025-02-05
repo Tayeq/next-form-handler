a93b5937a8e16d82b7cc002bf0dcba2bbc60d109
Tayeq <16797412+Tayeq@users.noreply.github.com>
2025-02-05 22:12:57 +0100

    bump version to 1.0.0, add FormWrapper component, and update dependencies

---
+++ src/components/form-wrapper.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import type {PropsWithChildren, ReactElement} from 'react';
+import type { FieldValues, UseFormReturn } from 'react-hook-form';
+import { Form } from '@/componencts/ui/form';
+import { createSubmitHandler, useFormHandler, type FormAction } from 'next-form-handler';
+
+export interface FormWrapperProps<T extends FieldValues> extends PropsWithChildren{
+    action: FormAction<T>;
+    form: UseFormReturn<T>;
+    onSuccess?: () => void;
+}
+
+export function FormWrapper<T extends FieldValues>({
+                                                       action,
+                                                       form,
+                                                       children,
+                                                       onSuccess: onBaseSuccess,
+                                                   }: FormWrapperProps<T>): ReactElement {
+    const onSuccess = (data: T) => {
+        form.reset(data);
+        onBaseSuccess?.();
+    };
+
+    const { formAction } = useFormHandler(action, onSuccess);
+
+    const onSubmit = form.handleSubmit(createSubmitHandler(formAction));
+
+    return (
+        <Form {...form}>
+            <form onSubmit={onSubmit}>{children}</form>
+        </Form>
+    );
+}
