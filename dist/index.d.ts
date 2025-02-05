ab59ce9c5ed2f633c1b8606068be61f06370005a
Tayeq <16797412+Tayeq@users.noreply.github.com>
2025-02-05 16:04:01 +0100

    remove zod dependency and clean up imports in index.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,9 @@
 import { FormAction, FormState } from "./types";
-import { z, ZodSchema } from "zod";
 type UseFormHandlerReturn<T = unknown> = FormState<T> & {
     formAction: (payload: FormData) => void;
     isPending: boolean;
 };
 export declare const useFormHandler: <T = unknown>(action: FormAction<T>, onSuccess?: (data: T) => void, onError?: (error: Error) => void) => UseFormHandlerReturn<T>;
 export declare function mapToFormData(data: Record<string, unknown>): FormData;
-export declare function createSubmitHandler<T extends ZodSchema>(formAction: (formData: FormData) => void): (data: z.infer<T>) => void;
+export declare function createSubmitHandler(formAction: (formData: FormData) => void): (data: Record<string, unknown>) => void;
 export {};
